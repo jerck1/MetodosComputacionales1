0f9e3eb2c3b95c96c0b68cf9ca0c96251bccb4df
jerck1 <johncafre@gmail.com>
2022-03-08 17:00:29 -0500

    subo act5

---
+++ Actividad_5/test.py
@@ -0,0 +1,12 @@
+def test1(*args):
+    try:
+        assert np.abs(integral_riemann(*args)-0.746824132812427)<1e-6
+        print("Resultado Correcto")
+    except:
+        print("Resultado Incorrecto")
+def test2(*args):
+    try:
+        assert np.abs(integral_trapecio(*args)-0.882081390762422)<1e-6
+        print("Resultado Correcto")
+    except:
+        print("Resultado Incorrecto")
