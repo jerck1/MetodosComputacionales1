5739142e1e40df10b5f95767032c0fecd83770ab
jerck1 <johncafre@gmail.com>
2022-03-14 17:07:25 -0500

    Cambios test.py

--- Actividad_5/test.py
+++ Actividad_5/test.py
@@ -1,10 +1,10 @@
 import numpy as np
 def test1(*args):
- #   try:
+   try:
      assert np.abs(integral_riemann(*args)-0.746824132812427)<1e-6
-#        print("Resultado Correcto")
-#    except:
-#        print("Resultado Incorrecto")
+        print("Resultado Correcto")
+    except:
+        print("Resultado Incorrecto")
 def test2(*args):
     try:
         assert np.abs(integral_trapecio(*args)-0.746824132812427)<1e-6
